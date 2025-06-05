0b3083e82a0bedb7e6aabd1f555df5eab0cb7918
orlinenjeuhou1 <108906@ecole-it.com>
2025-06-05 16:54:18 +0200

    Nettoyage de feature/client : suppression des dossiers backend, proxy et CI/CD

--- server/pizza/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Pizza
-
-class PizzaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pizza
-        fields = '__all__'

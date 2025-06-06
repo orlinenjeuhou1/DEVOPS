bebfecee941cb0ace0532c4b4ed0a8c1954473d4
orlinenjeuhou1 <108906@ecole-it.com>
2025-06-06 12:23:18 +0200

    Nettoyage : suppression de tous les fichiers sauf .github

--- server/pizza/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Pizza
-from .serializers import PizzaSerializer
-
-class PizzaViewSet(viewsets.ModelViewSet):
-    queryset = Pizza.objects.all()
-    serializer_class = PizzaSerializer
